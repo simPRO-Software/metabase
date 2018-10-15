7f76b1f63858d93ee34b1ccc925e5d90e0230cbb
thomas duval <thomas.duval@simpro.co>
2018-10-16 08:32:19 +1000

    change session time to 60min

--- src/metabase/config.clj
+++ src/metabase/config.clj
@@ -23,7 +23,7 @@
    ;; other application settings
    :mb-password-complexity "normal"
    :mb-version-info-url    "http://static.metabase.com/version-info.json"
-   :max-session-age        "20160"                                        ; session length in minutes (14 days)
+   :max-session-age        "60"                                        ; session length in minutes (14 days)
    :mb-colorize-logs       (str (not is-windows?))                        ; since PowerShell and cmd.exe don't support ANSI color escape codes or emoji,
    :mb-emoji-in-logs       (str (not is-windows?))                        ; disable them by default when running on Windows. Otherwise they're enabled
    :mb-qp-cache-backend    "db"})
