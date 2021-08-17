eccba2de23c9dbf25a7af693a09cf8df36713371
Lev Feldman <lev.feldman@simprogroup.com>
2021-08-17 11:35:12 +1000

    updated brand color

--- frontend/src/metabase/lib/colors.js
+++ frontend/src/metabase/lib/colors.js
@@ -12,7 +12,7 @@ export type ColorFamily = { [name: ColorName]: ColorString };
 // NOTE: KEEP SYNCRONIZED WITH COLORS.CSS
 /* eslint-disable no-color-literals */
 const colors = {
-  brand: "#004b8d",
+  brand: "#1976d2",
   "brand-light": "#DDECFA",
   accent1: "#88BF4D",
   accent2: "#A989C5",
