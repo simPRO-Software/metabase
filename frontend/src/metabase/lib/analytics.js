6db445ef18b9f30ff564938b0bfc7624a85bea9a
Lev Feldman <lev.feldman@simprogroup.com>
2021-08-17 10:32:34 +1000

    formatting

--- frontend/src/metabase/lib/analytics.js
+++ frontend/src/metabase/lib/analytics.js
@@ -1,16 +1,16 @@
 // Simple module for in-app analytics.  Currently sends data to GA but could be extended to anything else.
 const MetabaseAnalytics = {
   // track a pageview (a.k.a. route change)
-  trackPageView: function (url: string) {},
+  trackPageView: function(url: string) {},
 
   // track an event
-  trackEvent: function (
+  trackEvent: function(
     category: string,
     action?: ?string,
     label?: ?(string | number | boolean),
-    value?: ?number
-  ) {}
-}
+    value?: ?number,
+  ) {},
+};
 
 export default MetabaseAnalytics;
 
